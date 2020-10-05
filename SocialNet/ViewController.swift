e544bf28c84fd04f05e597e42822d71a25d880ac
Roman <egrams@mail.ru>
2020-10-05 16:43:23 +0400

    Initial Commit

---
+++ SocialNet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialNet
+//
+//  Created by Роман Евтюхин on 05.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
